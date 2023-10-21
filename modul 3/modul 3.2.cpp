a900112d603c4f5700805bbf50919fe908ebdb2e
Neng Nur Aeni <nuraenineng06@gmail.com>
2023-10-21 15:00:27 +0700

    first commit

--- modul 3/modul 3.2.cpp
+++ modul 3/modul 3.2.cpp
@@ -2,17 +2,8 @@
 
 int main()
 {
-	int suhuCelcius;
-	float suhuFahrenheit,suhuReamur;
+	printf ("NAMA  : Nur\n");
+    printf ("NIM     : 00214150\n");
+	printf ("JURUSAN : Teknik Informatika\n");
 	
-	printf("Menghitung luas dan keliling segitiga");
-	printf("\n====================================");
-	printf("\nMasukkan Suhu Celcius =");
-	scanf(" %i", &suhuCelcius);
-	
-	suhuFahrenheit = suhuCelcius * 1.8 + 32;
-	suhuReamur = suhuCelcius * 0.8;
-	
-	printf("\nJika Dalam Suhu Fahrenheit = %f", suhuFahrenheit);
-	printf("\nJika Dalam Suhu Reamur = %f", suhuReamur);
 }
