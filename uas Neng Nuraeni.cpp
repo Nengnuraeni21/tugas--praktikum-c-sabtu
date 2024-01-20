6c1240c641afa1448ee48755ae4991f1576727a2
Neng Nur Aeni <nuraenineng06@gmail.com>
2024-01-20 15:47:49 +0700

    first commit

---
+++ uas Neng Nuraeni.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// Fungsi untuk menghitung nilai tengah dari ketebalan pengelasan
+int hitungNilaiTengah(int ketebalan) {
+    return ketebalan / 2;
+}
+
+// Fungsi untuk simulasi pengelasan
+void prosesPengelasan(int ketebalan, int waktu) {
+    printf("Melakukan pengelasan untuk ketebalan %d mm selama %d detik.\n", ketebalan, waktu);
+}
+
+// Fungsi untuk menjalankan proses pengelasan dengan array ketebalan
+void jalankanProsesPengelasan(int ketebalanPengelasan[], int jumlahKetebalan) {
+    printf("Tombol Start ditekan. Motor listrik utama berputar 180 derajat.\n");
+
+    for (int i = 0; i < jumlahKetebalan; i++) {
+        int nilaiTengah = hitungNilaiTengah(ketebalanPengelasan[i]);
+
+        printf("Motor listrik utama berputar menuju nilai tengah %d.\n", nilaiTengah);
+        sleep(1); 
+        printf("Motor listrik lengan tungkai berputar 45 derajat.\n");
+        sleep(1); 
+        printf("Motor listrik jari robot mendekati objek.\n");
+        sleep(1); 
+        prosesPengelasan(ketebalanPengelasan[i], i + 2);
+        sleep(1);
+    }
+}
+
+int main() {
+    int ketebalanPengelasan[] = {6, 8, 12, 18, 24, 30, 36, 42, 49, 50};
+    int jumlahKetebalan = sizeof(ketebalanPengelasan) / sizeof(ketebalanPengelasan[0]);
+
+    jalankanProsesPengelasan(ketebalanPengelasan, jumlahKetebalan);
+
+    return 0;
+}
